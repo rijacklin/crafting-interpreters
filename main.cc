6665796aa3499026bf7926102416fea9fc2c5de5
Richard Jacklin <rijacklin1@gmail.com>
2025-11-06 19:07:33 -0500

    Added comments to simple doubly-linked list implementation

--- main.cc
+++ main.cc
@@ -4,6 +4,7 @@ int main() {
   // Create empty list
   DLList *list = new DLList{nullptr, nullptr};
 
+  // instantiate three empty nodes
   DLNode *firstNode = new DLNode{
       .next = nullptr, .prev = nullptr, .data = new std::string("A")};
   DLNode *secondNode = new DLNode{
@@ -11,13 +12,17 @@ int main() {
   DLNode *thirdNode = new DLNode{
       .next = nullptr, .prev = nullptr, .data = new std::string("C")};
 
-  // insert two nodes
+  // insert the three nodes into the list
   insert(list, firstNode);
   insert(list, secondNode);
   insert(list, thirdNode);
 
+  // print the current list
   print(list, length(list));
 
+  // remove second node in list
   removeNode(list, find(list, 1));
+
+  // print list again after removal
   print(list, length(list));
 }
